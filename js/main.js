c0cfb6b9e82614a9ab52c03da99946c9455cbecd
LucaBrivio183 <l.brivio4@gmail.com>
2023-02-28 15:16:51 +0100

    feat: excercise done

--- js/main.js
+++ js/main.js
@@ -2,4 +2,29 @@
 
 'use strict'
 
-alert ('ciao')
+//define array for shopping list items
+
+const shoppingList = [
+    'proteine',
+    'creatina',
+    'pollo',
+];
+
+//define variable for html item
+
+const toBuyList = document.querySelector('.unstyled-list');
+
+//define i variable for index usage
+
+let i=0;
+
+//cycle troughevery item and append in ul list
+
+while  (i<shoppingList.length) {
+    console.log(shoppingList[i])
+    const toBuyListItem = document.createElement('li');
+    toBuyListItem.append (shoppingList[i]);
+    toBuyList.append(toBuyListItem);
+    i++;
+}
+
